3a3f83af387cb29770c7da414b67f439c4c1d50b
Jochen <jochen.handels@web.de>
2019-04-04 18:08:13 +0200

    Using ZIP file instead of plain folder

--- Notebooks/load_petimages.bat
+++ Notebooks/load_petimages.bat
@@ -1,1 +1,2 @@
-python tools\load_image_data.py petimages cat dog --shape=50,50,3  %*
+rem download kagglecatsanddogs_3367a.zip from https://www.microsoft.com/en-us/download/details.aspx?id=54765
+python tools\load_image_data.py kagglecatsanddogs_3367a.zip cat:petimages\cat dog:petimages\dog --shape=50,50,3 --out=petimages %*
--- Notebooks/load_petimages.sh
+++ Notebooks/load_petimages.sh
@@ -1,3 +1,4 @@
 #!/bin/env sh
 
-python tools/load_image_data.py petimages cat dog --shape=50,50 $@
+# download kagglecatsanddogs_3367a.zip from https://www.microsoft.com/en-us/download/details.aspx?id=54765
+python tools/load_image_data.py kagglecatsanddogs_3367a.zip cat:petimages/cat dog:petimages/dog --shape=50,50,3 --out=petimages $@
